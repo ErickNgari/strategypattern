78dcb7e38fb6060b1f9669fcffdf67b8e9bc3105
Erick Leopard <eric.ngari@lctafrica.net>
2023-04-17 15:56:26 +0300

    Strategy Pattern

---
+++ src/main/kotlin/com/strategypattern/strategypattern/StrategypatternApplication.kt
@@ -0,0 +1,11 @@
+package com.strategypattern.strategypattern
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StrategypatternApplication
+
+fun main(args: Array<String>) {
+	runApplication<StrategypatternApplication>(*args)
+}
